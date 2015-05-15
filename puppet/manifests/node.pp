4951229c7b76daa060b8dd2461c90b999bd30e69
Chris Mague <github@mague.com>
2015-05-14 18:48:51 -0700

    listen on shared interfaces

--- puppet/manifests/node.pp
+++ puppet/manifests/node.pp
@@ -4,10 +4,12 @@ include docker
 include stdlib
 
 class { '::consul':
-  config_hash => {
+  config_hash     => {
     'data_dir'    => '/opt/consul',
     'datacenter'  => 'vagrant',
     'log_level'   => 'INFO',
+    'client_addr' => $::ipaddress_eth1,
+    'bind_addr' => $::ipaddress_eth1,
     'node_name'   => $::hostname,
     'retry_join'  => ['172.16.3.101'],
   }
